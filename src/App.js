c21330843f5afdd1411df1b339df5494024d8e3b
Gopal-cloud-repo <gbera435@gmail.com>
2026-02-18 18:04:33 +0530

    Create main React component for FF Montage Editor

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>FF Montage Editor</h1>
+            {/* Add your montage editing components here */}
+        </div>
+    );
+};
+
+export default App;
